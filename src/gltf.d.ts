c1604744a2fb135cf2b9da0a058a814ee30ca9a9
domgell <domgelland@gmail.com>
2024-11-01 19:09:00 +0100

    WIP

--- src/gltf.d.ts
+++ src/gltf.d.ts
@@ -1,6 +1,13 @@
-export type ID = number
+/**
+ * Based on GLTF specification:
+ * https://registry.khronos.org/glTF/specs/2.0/glTF-2.0.html#properties-reference
+ */
 
-const enum AccessorComponentType {
+export type TODO = any
+
+// ----------------------------- Accessor ------------------------------
+
+export enum AccessorComponentType {
     BYTE = 5120,
     UNSIGNED_BYTE = 5121,
     SHORT = 5122,
@@ -17,6 +24,80 @@ export type AccessorType =
     | "MAT2"
     | "MAT3"
     | "MAT4"
+
+export type Accessor = {
+    /**
+     * The index of the bufferView.
+     */
+    bufferView?: number,
+    /**
+     * The offset relative to the start of the buffer view in bytes.
+     */
+    byteOffset?: number
+    /**
+     * The datatype of the accessor's components.
+     */
+    componentType: AccessorComponentType,
+    /**
+     * Specifies whether integer data values are normalizd before usage.
+     */
+    normalized?: boolean,
+    /**
+     * The number of elements referenced by this accessor.
+     */
+    count: number,
+    /**
+     * Specifies if the accessor's elements are scalars, vectors, or matrices.
+     */
+    type: AccessorType,
+    /**
+     * Maximum value of each component in this accessor. For example, maximum x, y, z components of a Vector3.
+     */
+    max?: Translation | Rotation | Scale | Matrix,
+    /**
+     * Minimum value of each component in this accessor. For example, minimum x, y, z components of a Vector3.
+     */
+    min?: Translation | Rotation | Scale | Matrix,
+    /**
+     * Sparse storage of elements that deviate from their initialization value.
+     */
+    sparse?: TODO
+    /**
+     * User-defined name of this object
+     */
+    name?: string,
+    /**
+     * Object with extension-specific objects
+     */
+    extensions?: Record<string, TODO>,
+    /**
+     * Application-specific data.
+     */
+    extras?: TODO
+}
+
+export type AccessorSparse = {
+    /**
+     * Number of deviating accessor values stored in the sparse array.
+     */
+    count: number
+    /**
+     * An object pointing to a buffer view containing the indices of deviating accessor values. The number of indices is equal to `count`. Indices *MUST* strictly increase.
+     */
+    indices: TODO,
+    /**
+     * An object pointing to a buffer view containing the deviating accessor values.
+     */
+    values: TODO,
+    /**
+     * Object with extension-specific objects
+     */
+    extensions?: Record<string, TODO>,
+    /**
+     * Application-specific data.
+     */
+    extras?: TODO
+}
 
 // TODO
 export type Translation = [number, number, number]
@@ -59,15 +140,15 @@ export type AnimationSampler = {
     /**
      * The index of an accessor containing keyframe input values, e.g., time
      */
-    input: number;
+    input: number,
     /**
      * Interpolation algorithm
      */
-    interpolation?: AnimationSamplerInterpolation;
+    interpolation?: AnimationSamplerInterpolation,
     /**
      * The index of an accessor, containing keyframe output values
      */
-    output: number;
+    output: number,
 }
 
 export type AnimationSamplerInterpolation =
@@ -79,11 +160,23 @@ export type Animation = {
     /**
      * An array of animation channels. An animation channel combines an animation sampler with a target property being animated. Different channels of the same animation **MUST NOT** have the same targets.
      */
-    "channels": AnimationChannel[];
+    channels: AnimationChannel[],
     /**
      * An array of animation samplers. An animation sampler combines timestamps with a sequence of output values and defines an interpolation algorithm.
      */
-    "samplers": AnimationSampler[];
+    samplers: AnimationSampler[],
+    /**
+     * User-defined name of this object
+     */
+    name?: string,
+    /**
+     * Object with extension-specific objects
+     */
+    extensions?: Record<string, TODO>,
+    /**
+     * Application-specific data.
+     */
+    extras?: TODO
 }
 
 // ------------------------------ Texture ------------------------------
@@ -116,7 +209,7 @@ export type Texture = {
     /**
      * The index of the image used by this texture
      */
-    source: number;
+    source: number,
 }
 
 // TODO: TextureInfo
@@ -127,24 +220,25 @@ export type Sampler = {
     /**
      * Magnification filter.  Valid values correspond to WebGL enums: 9728 (NEAREST) and 9729 (LINEAR)
      */
-    magFilter?: TextureMagFilter;
+    magFilter?: TextureMagFilter,
     /**
      * Minification filter.  All valid values correspond to WebGL enums
      */
-    minFilter?: TextureMinFilter;
+    minFilter?: TextureMinFilter,
     /**
      * S (U) wrapping mode.  All valid values correspond to WebGL enums
      */
-    wrapS?: TextureWrapMode;
+    wrapS?: TextureWrapMode,
     /**
      * T (V) wrapping mode.  All valid values correspond to WebGL enums
      */
-    wrapT?: TextureWrapMode;
+    wrapT?: TextureWrapMode,
 }
 
 
 // ------------------------------ Material -----------------------------
 
+export type Material = {}
 
 // ------------------------------ Camera -------------------------------
 
@@ -199,19 +293,30 @@ export type Camera =
 
 export type MeshPrimitiveMode = {} // TODO
 
+export type MeshPrimitiveAttributes = {
+    POSITION: number,
+    TEXCOORD_0?: number,
+    TEXCOORD_1?: number,
+    NORMAL?: number
+    COLOR_0?: number,
+    JOINTS_0?: number,
+    WEIGHTS_0?: number,
+}
+
 export type MeshPrimitive = {
     /**
-     * A plain JSON object, where each key corresponds to a mesh attribute semantic and each value is the index of the accessor containing attribute's data.
+     * // TODO
      */
-    attributes: Record<string, number>,
+    attributes: MeshPrimitiveAttributes
+
     /**
      * The index of the accessor that contains the vertex indices.
      */
-    indices?: number;
+    indices?: number,
     /**
      * The index of the material to apply to this primitive when rendering.
      */
-    material?: number;
+    material?: number,
     /**
      * The topology type of primitives to render.
      */
@@ -238,17 +343,17 @@ export type Mesh = {
 
 export type Skin = {
     /**
-     * The index of the accessor containing the floating-point 4x4 inverse-bind matrices.  The default is that each matrix is a 4x4 identity matrix, which implies that inverse-bind matrices were pre-applied
+     * The index of the accessor containing the floating-point 4x4 inverse-bind matrices. The default is that each matrix is a 4x4 identity matrix, which implies that inverse-bind matrices were pre-applied
      */
-    inverseBindMatrices?: number;
+    inverseBindMatrices?: number,
     /**
      * The index of the node used as a skeleton root. When undefined, joints transforms resolve to scene root
      */
-    skeleton?: number;
+    skeleton?: number,
     /**
-     * Indices of skeleton nodes, used as joints in this skin.  The array length must be the same as the count property of the inverseBindMatrices accessor (when defined)
+     * Indices of skeleton nodes, used as joints in this skin. The array length must be the same as the count property of the inverseBindMatrices accessor (when defined)
      */
-    joints: number[];
+    joints: number[],
 
     name?: string,
 }
@@ -256,7 +361,7 @@ export type Skin = {
 // ------------------------------- Node --------------------------------
 
 /**
- * A node in the node hierarchy.  When the node contains `skin`, all `mesh.primitives` **MUST** contain `JOINTS_0` and `WEIGHTS_0` attributes.  A node **MAY** have either a `matrix` or any combination of `translation`/`rotation`/`scale` (TRS) properties. TRS properties are converted to matrices and postmultiplied in the `T * R * S` order to compose the transformation matrix; first the scale is applied to the vertices, then the rotation, and then the translation. If none are provided, the transform is the identity. When a node is targeted for animation (referenced by an animation.channel.target), `matrix` **MUST NOT** be present.
+ * A node in the node hierarchy.  When the node contains `skin`, all `mesh.primitives` **MUST** contain `JOINTS_0` and `WEIGHTS_0` attributes.  A node **MAY** have either a `matrix` or any combination of `translation`/`rotation`/`scale` (TRS) properties. TRS properties are converted to matrices and postmultiplied in the `T * R * S` order to compose the transformation matrix, first the scale is applied to the vertices, then the rotation, and then the translation. If none are provided, the transform is the identity. When a node is targeted for animation (referenced by an animation.channel.target), `matrix` **MUST NOT** be present.
  */
 
 // TODO type unions
@@ -264,40 +369,40 @@ export type Node = {
     /**
      * The index of the camera referenced by this node.
      */
-    camera?: number;
+    camera?: number,
     /**
      * The indices of this node's children.
      */
-    children?: number[];
+    children?: number[],
     /**
      * The index of the skin referenced by this node.
      */
-    skin?: number;
+    skin?: number,
     /**
      * A floating-point 4x4 transformation matrix stored in column-major order.
      */
-    matrix?: [number, number, number, number, number, number, number, number, number, number, number, number, number, number, number, number];
+    matrix?: [number, number, number, number, number, number, number, number, number, number, number, number, number, number, number, number],
     /**
      * The index of the mesh in this node.
      */
-    mesh?: number;
+    mesh?: number,
     /**
      * The node's unit quaternion rotation in the order (x, y, z, w), where w is the scalar.
      */
-    rotation?: [number, number, number, number];
+    rotation?: [number, number, number, number],
     /**
      * The node's non-uniform scale, given as the scaling factors along the x, y, and z axes.
      */
-    scale?: [number, number, number];
+    scale?: [number, number, number],
     /**
      * The node's translation along the x, y, and z axes.
      */
-    translation?: [number, number, number];
+    translation?: [number, number, number],
     /**
      * The weights of the instantiated morph target. The number of array elements **MUST** match the number of morph targets of the referenced mesh. When defined, `mesh` **MUST** also be defined.
      */
-    weights?: number[];
-    name?: string;
+    weights?: number[],
+    name?: string,
 }
 
 
@@ -307,17 +412,32 @@ export type Scene = {
     /**
      * The indices of each root node
      */
-    nodes: number[];
+    nodes: number[],
+    name?: string,
 }
 
 
-// ------------------------------- GLTF -------------------------------
+// ------------------------------ Header ------------------------------
 
 /**
- * The root object for a glTF asset.
+ * JSON part of the GLTF/GLB file
  */
-export type GLTF = {
+export type Header = {
     // TODO
+    accessors: Accessor[],
+    buffers,
+    bufferViews,
+    meshes: Mesh[],
+    nodes: Node[],
+    scene: number // ?
+    scenes: Scene[]
 }
+
+export type Asset = {
+    generator: string,
+    version: string,
+}
+
+
 
 
